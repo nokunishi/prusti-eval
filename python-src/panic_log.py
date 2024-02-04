ab02d8ad0ed9fd4a13e51d28250fb398722e22bf
nokunish <nokunish@student.ubc.ca>
2024-02-04 01:16:06 -0800

    organized file structure

---
+++ python-src/panic_log.py
@@ -0,0 +1,80 @@
+import os 
+import datetime
+import json
+
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+parent_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
+panic_report_dir = os.path.join(parent_dir, "log", "panic_report")
+panic_reports = os.listdir(panic_report_dir)
+
+date_ = str(datetime.datetime.now()).split(" ")
+date = date_[0] + "-" + date_[1]
+panic_summary_dir = os.path.join(parent_dir, "log", "panic_summary")
+print(panic_summary_dir )
+panic_summary_path = panic_summary_dir + "/" + date + ".json"
+
+class Stats:
+    reason = {}
+    fn = {}
+
+
+def eval():
+    s = Stats()
+
+    for panic in panic_reports:
+        with open(os.path.join(panic_report_dir, panic), "r") as f: 
+            lines = f.readlines()
+            i = 0
+
+            while i < len(lines) - 3:
+                line = lines[i]
+                
+                if "thread 'rustc' panicked at" in line:        
+                    lines[i+1] = lines[i+1].strip()    
+                    if  "called `Result::unwrap()` on an `Err` value:" in lines[i+1]:
+                        lines[i+1] =  "called `Result::unwrap()` on an `Err` value:"
+                    if lines[i+1] not in s.reason:
+                        s.reason[lines[i+1]] = {
+                            "num": 1,
+                            "crates": [panic[:-4]]
+                        }
+                    else:
+                        if panic[:-4] not in s.reason[lines[i+1]]["crates"]:
+                            s.reason[lines[i+1]]["num"] += 1
+                            s.reason[lines[i+1]]["crates"].append(panic[:-4])
+
+                if "called" in line:
+                    words = line.split(" ");
+                    fn_key = [];
+
+                    for word in words:
+                        if "`" in word and "::" in word:
+                            word = word.replace("`", "")
+                            module = word.split("::")[0]
+
+                            if module not in s.fn:
+                                s.fn[module] = {
+                                    "num_panicked_fns": 1,
+                                    "type": [line.strip()]
+                                }
+                            else:
+                                if not line.strip() in s.fn[module]["type"]:
+                                    s.fn[module]["num_panicked_fns"] += 1
+                                    s.fn[module]["type"] = line.strip()
+
+                i += 1
+
+    stats = {
+        "total_num_crates_paniced": len(panic_reports),
+        "reason_num": len(s.reason),
+        "reason": s.reason,
+        "panicky_fns": s.fn
+    }
+
+    json_stats = json.dumps(stats, indent= 8)
+    with open(panic_summary_path, "w") as f:
+        print("writing to summary")
+        f.write(json_stats)
+
+if __name__ == '__main__':
+    eval()
---
+++ python-src/run_x.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import shutil
+
+tmp_dir = os.listdir("/tmp")
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+parent_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
+log_dir = os.path.join(parent_dir, "log")
+log = os.listdir(log_dir)
+archive_dir = os.path.join(log_dir, "archive")
+archive = os.listdir(archive_dir)
+
+""" python3 run_x.py num rerun"""
+
+def zip():
+    if (len(archive) > 100) :   
+        zipped_dist = os.path.join(log_dir, "zipped", 'archive_' + str(len(archive)))
+        shutil.make_archive(base_name=zipped_dist, format='zip', root_dir=os.path.join(log_dir, "archive"))
+
+        if os.path.exists(zipped_dist + ".zip"):
+            shutil.rmtree(archive_dir)
+            os.mkdir(archive_dir)
+
+
+def setup():
+    err_dir = ""
+    err_reports = []  
+
+    if "err_report" in log:
+        err_dir = os.path.join(log_dir, "err_report")
+        err_reports = os.listdir(err_dir)
+
+    if len(sys.argv) < 2:
+        print("incorrect number of args")
+        exit()
+
+    os.system("python3 ./x.py run --bin setup_crates cratelist" ) 
+    print("setup complete")
+    return err_reports
+
+def reset_tmp():
+    for tmp in tmp_dir:
+        if ".crate" in tmp:
+            os.remove('/tmp/' + tmp)
+            if tmp[:-6] in tmp_dir:
+                shutil.rmtree('/tmp/' + tmp[:-6])
+
+def main():
+    
+    if "reset" in sys.argv: 
+        reset_tmp()
+        return 
+
+    zip()
+    err_reports = setup()
+    
+    i = 0
+    num = int(sys.argv[1])
+
+    while i < num and i < len(tmp_dir): 
+        crate_name = tmp_dir[i][:-6] + ".txt" 
+        report_name = tmp_dir[i][:-6] + ".json"
+
+        if crate_name in archive or report_name in err_reports:
+            print("Prusti already ran on crate:" + tmp_dir[i])
+            os.remove('/tmp/' + tmp_dir[i])
+            shutil.rmtree('/tmp/' + tmp_dir[i][:-6])
+            num += 1
+    
+        else:
+            if ".crate" in tmp_dir[i]:
+                print("running on: " + tmp_dir[i])
+                try: 
+                    os.system("python3 ./x.py run --bin run_prusti clippy &> " + archive_dir + "/" + crate_name + " " + crate_name)
+                except:
+                    print("failed to run Prusti on " + crate_name)
+            else:
+                num += 1
+        i += 1
+
+    import err_log
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+          
+
