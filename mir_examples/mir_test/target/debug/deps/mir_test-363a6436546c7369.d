45582fd3d135ae643d9d3d572d767123edaa9d88
nokunish <133714697+nokunish@users.noreply.github.com>
2024-02-12 04:39:14 -0800

    Delete mir_examples/mir_test directory

--- mir_examples/mir_test/target/debug/deps/mir_test-363a6436546c7369.d
+++
@@ -1,5 +0,0 @@
-/Users/hatsusaiokunishi/UBC/2023/cs448/source/prusti-eval/mir_examples/mir_test/target/debug/deps/mir_test-363a6436546c7369.rmeta: src/main.rs
-
-/Users/hatsusaiokunishi/UBC/2023/cs448/source/prusti-eval/mir_examples/mir_test/target/debug/deps/mir_test-363a6436546c7369.d: src/main.rs
-
-src/main.rs:
--- mir_examples/mir_test/target/debug/deps/mir_test-d2cab4219c450389.d
+++
@@ -1,5 +0,0 @@
-/Users/hatsusaiokunishi/UBC/2023/cs448/source/prusti-eval/mir_examples/mir_test/target/debug/deps/mir_test-d2cab4219c450389.rmeta: src/main.rs
-
-/Users/hatsusaiokunishi/UBC/2023/cs448/source/prusti-eval/mir_examples/mir_test/target/debug/deps/mir_test-d2cab4219c450389.d: src/main.rs
-
-src/main.rs:
