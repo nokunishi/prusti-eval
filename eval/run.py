7a89f5484c6e5d809a2d271a927fe7ffd0aa3de0
niina.okunishi@gmail.com <nokunish@student.ubc.ca>
2024-04-12 13:37:44 -0700

    backup

--- eval/run.py
+++ eval/run.py
@@ -35,11 +35,11 @@ def equal(r, r_p):
     
     # explicit panic
     if "unreachable!(..)" in r_p and \
-         'const "internal error: entered unreachable code"' in r:
+         'internal error: entered unreachable code' in r:
         return True
-    if "panic!(..)" in r_p and 'const "explicit panic"' in r:
+    if "panic!(..)" in r_p and 'explicit panic' in r:
         return True
-    if "unimplemented!(..)" in r_p and 'const "not implemented"' in r:
+    if "unimplemented!(..)" in r_p and 'not implemented' in r:
         return True
     if "index" in r_p and "index" in r:
         return True
@@ -60,8 +60,8 @@ def print_crates(k):
         print(c, sep="\n")
 
 def mir_us(mir, list):
-    us = []
-    other = []
+    us = {}
+    other = {}
 
     for obj in c.unsupported(mir):
         file_us = [*obj.keys()][0].split(":")[0]
@@ -75,7 +75,7 @@ def mir_us(mir, list):
             list_ = line[[*line.keys()][0]]
             
             for l_ in list_:
-                fn = l_["fn"].split("-")[1]
+                fn = l_["fn"].split("-")[-1].split("::")[-1]
                 file = l_["path"].split(":")[0]
                 start = int(l_["path"].split(".rs:")[1].split(":")[0])
                 end = int(l_["path"].split(".rs:")[1].split(" ")[1].split(":")[0])
@@ -85,17 +85,26 @@ def mir_us(mir, list):
                     start, end = c.get_endlns(tmp, fn)
                 
                 if start <= ln_us and ln_us <= end and file_us == file:
-                    o = {"fn": l_["fn"], "path": l_["path"], "l_no": ln_us, "rsn": obj[[*obj.keys()][0]]}
-                    if o not in us:
-                        us.append(o)    
+                    rsn =  obj[[*obj.keys()][0]]
+                    o = {"fn": l_["fn"], "path": l_["path"], "l_no": ln_us}
+                    if rsn in us:
+                        if o not in us[rsn]:
+                            us[rsn].append(o)
+                    else:
+                        us[rsn] = [o]    
                 else:
-                    o = {"fn": l_["fn"], "path": l_["path"], "mir": l_["mir"]}
-                    other.append(o)
-
+                    o = {"fn": l_["fn"], "path": l_["path"]}
+                    if [*line.keys()][0] in other:
+                        if o not in other[[*line.keys()][0]]:
+                            other[[*line.keys()][0]].append(o)
+                    else:
+                        other = {
+                            [*line.keys()][0]: [o]
+                        }
     return us, other
 
 def mir_ve(mir, p_lines):
-    eq = []
+    eq = {}
     ne = []
     mir_only = []
 
@@ -104,7 +113,7 @@ def mir_ve(mir, p_lines):
             r = [*obj.keys()][0]
             for l in obj[r]:
                 file = l["path"].split(":")[0]
-                fn = l["fn"].split("-")[1]
+                fn = l["fn"].split("-")[-1].split("::")[-1]
         
                 start =  int(l["path"].split(".rs:")[1].split(" ")[0].split(":")[0])
                 end =  int(l["path"].split(" ")[1].split(":")[0])
@@ -122,15 +131,10 @@ def mir_ve(mir, p_lines):
                     if start <= ln_p and ln_p <= end and file_p == file:
                         inPrusti = True
                         if equal(r, r_p):
-                            inlist = False
-                            for obj in eq:
-                                if [*obj.keys()][0] == r:
-                                    obj[r].append({"fn": fn, "path": l["path"]})
-                                    inlist = True
-
-                            if not inlist:
-                                eq.append(
-                                    {r: [{"fn": l["fn"], "path": l["path"]}] })
+                            if r in eq:
+                                eq[r].append({"fn": fn, "path": l["path"]})
+                            else:
+                                eq[r] =  [{"fn": l["fn"], "path": l["path"]}] 
                         else:
                             ne.append({"fn": l["fn"], "path": l["path"], "mir":r, "prusti": r_p})
 
@@ -139,16 +143,20 @@ def mir_ve(mir, p_lines):
                     inlist = False
                     for obj in mir_only:
                         if [*obj.keys()][0] == r:
-                            obj[r].append({"fn": l["fn"], "path": l["path"], "mir": r})
+                            obj[r].append({"fn": l["fn"], "path": l["path"]})
                             inlist = True
 
                     if not inlist:
-                        mir_only.append({r: [{"fn": l["fn"], "path": l["path"], "mir": r}] })
+                        mir_only.append({r: [{"fn": l["fn"], "path": l["path"]}] })
 
         m_.close()                             
     return eq, ne, mir_only                
 
 def write(mir, eq, ne, us, other):
+    eq = dict(sorted(eq.items(),  key=lambda x: len(x), reverse=True))
+    us = dict(sorted(us.items(),  key=lambda x: len(x), reverse=True))
+    other = dict(sorted(other.items(),  key=lambda x: len(x), reverse=True))
+    
     with open(mir, "r") as m_:
         m = json.load(m_)
         f_total = m["fn_total"]
@@ -192,6 +200,11 @@ def run():
     while i < n and i < len(mirs):
         if setMIR:
             mir = mirs[i]
+
+        #if mir in os.listdir(w.e_c):
+        #    print("comparison report already exists for "+ mir)
+        #    i += 1
+        #    continue
         if mir in m_rerun:
             m = os.path.join(w.m_rerun, mir)
         else:
--- eval/summary.py
+++ eval/summary.py
@@ -8,47 +8,27 @@ class Stat:
     match_t = 0
     mir_l = {}
     mir_t = 0
-    us = 0
+    mmatch_t = 0
+    us_l = {}
+    us_t = 0
 
-def count_match(s, crate, list):
-    for obj in list:
-        rsn = [*obj.keys()][0]
-
-        if rsn in s.match_l:
-            s.match_l[rsn]["count"] += len(obj[rsn])
-        else:
-            o = {
-                "crate": crate,
-                "fn": obj[rsn][0]["fn"],
-                "path": obj[rsn][0]["path"],
-            }
-
-            s.match_l[rsn] = {
-                "count": len(obj[rsn]),
-                "i.e.": o,
-            }
-
-        s.match_t += len(obj[rsn])
-    return s.match_l, s.match_t
-
-def count_mir(s, crate, list):
-    for obj in list:
-        rsn = obj["mir"]
-
-        if rsn in s.mir_l:
-            s.mir_l[rsn]["count"] += 1
-        else:
-            o = {
+def count(acc, t, crate, list):
+    for rsn in list:
+        for o in list[rsn]:
+            if rsn in acc:
+                acc[rsn]["count"] += 1
+            else:
+                o_ = {
                 "count": 1,
                 "i.e.": {
                     "crate": crate,
-                    "fn": obj["fn"],
-                    "path": obj["path"]
+                    "fn": o["fn"],
+                    "path": o["path"]
+                    }
                 }
-            }
-            s.mir_l[rsn] = o
-        s.mir_t += 1
-    return s.mir_l, s.mir_t
+                acc[rsn] = o_
+            t += 1
+    return acc, t
 
 
 def run():
@@ -57,19 +37,23 @@ def run():
     for r in os.listdir(w.e_c):
         with open(os.path.join(w.e_c, r), "r") as f_:
             f = json.load(f_)
-            s.match_l, s.match_t = count_match(s, r.replace(".json", ""), f["match"])
-            s.mir_l, s.mir_t = count_mir(s, r.replace(".json", ""), f["mir_only"])
-            s.us += len(f["unsupported"])
+            s.match_l, s.match_t = count(s.match_l, s.match_t, r.replace(".json", ""), f["match"])
+            s.mir_l, s.mir_t = count(s.mir_l, s.mir_t, r.replace(".json", ""), f["mir_only"])
+            s.us_l, s.us_t =  count(s.us_l, s.us_t, r.replace(".json", ""), f["unsupported"])
+            s.mmatch_t += len(f["mismatch"])
 
     
     s.match_l =  dict(sorted(s.match_l.items(),  key=lambda x: x[1]["count"], reverse=True))
     s.mir_l = dict(sorted(s.mir_l.items(),  key=lambda x: x[1]["count"], reverse=True))
+    s.us_l = dict(sorted(s.us_l.items(),  key=lambda x: x[1]["count"], reverse=True))
 
     obj = {
         "crate": len(os.listdir(w.e_c)),
-        "unsupported": s.us,
         "match_total": s.match_t,
         "match": s.match_l,
+        "us_total": s.us_t,
+        "us": s.us_l, 
+        "mismatch": s.mmatch_t,
         "mir_only_total": s.mir_t,
         "mir_only_l": s.mir_l
     }
