4dc335278f45a5ba14a5591e6e2cc6d5bde2bab0
niina.okunishi@gmail.com <nokunish@student.ubc.ca>
2024-04-13 19:23:35 -0700

    updated examples

--- mir/examples/div_mod/test.rs
+++ mir/examples/div_mod/test.rs
@@ -1,12 +1,25 @@
-pub fn div1(x: i32, y:i32){
+pub fn div_u1(x: u32, y:u32){
     x / y;
 }
 
-pub fn div2(x: i32){
+pub fn div_u2(x: u32){
     x / 9;
 }
 
-pub fn div3(x: i32){
+pub fn div_u3(x: u32){
+    3 / x;
+}
+
+
+pub fn div_i1(x: i32, y:i32){
+    x / y;
+}
+
+pub fn div_i2(x: i32){
+    x / 9;
+}
+
+pub fn div_i3(x: i32){
     3 / x;
 }
 
@@ -22,15 +35,27 @@ pub fn div_overflow(){
     i32::MIN / -1;
 }
 
-pub fn mod1(x: u32, y:u32) {
+pub fn mod_u1(x: u32, y:u32) {
     x % y;
 }
 
-pub fn mod2(x:u32) {
+pub fn mod_u2(x:u32) {
     8 % x;
 }
 
-pub fn mod3(x:u32) {
+pub fn mod_u3(x:u32) {
+    x % 2;
+}
+
+pub fn mod_i1(x: i32, y:i32) {
+    x % y;
+}
+
+pub fn mod_i2(x:i32) {
+    8 % x;
+}
+
+pub fn mod_i3(x:i32) {
     x % 2;
 }
 
@@ -42,9 +67,4 @@ pub fn mod_safe() {
 
 pub fn mod_overflow() {
     i32::MIN % -1;
-}
-
-
-pub fn mod_zero() {
-    8 % 0;
 }
