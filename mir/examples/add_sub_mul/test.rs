4e95dd209a043fd9b592a23f67939877c94cface
niina.okunishi@gmail.com <nokunish@student.ubc.ca>
2024-04-14 18:42:14 -0700

    updated examples

--- mir/examples/add_sub_mul/test.rs
+++ mir/examples/add_sub_mul/test.rs
@@ -1,6 +1,10 @@
 use std::u32::MAX;
 
-fn add(x:i32, y:i32) {
+fn add_i(x:i32, y:i32) {
+   let _ = x + y;
+}
+
+fn add_u(x:u32, y:u32) {
    let _ = x + y;
 }
 
@@ -12,7 +16,11 @@ fn add_overflow() {
    let _ = 1 + i32::MAX;
 }
 
-fn sub(x:i32, y:i32) {
+fn sub_i(x:i32, y:i32) {
+   let _ = x - y;
+}
+
+fn sub_u(x:u32, y:u32) {
    let _ = x - y;
 }
 
@@ -24,7 +32,11 @@ fn sub_underflow() {
    let _ = -1 - i32::MAX;
 }
 
-fn mul(x:i32, y:i32) {
+fn mul_i(x:i32, y:i32) {
+   let _ = x * y;
+}
+
+fn mul_u(x:u32, y:u32) {
    let _ = x * y;
 }
 
